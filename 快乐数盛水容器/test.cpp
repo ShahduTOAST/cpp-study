6caec86fbdf3b345f2a08c12a35f00bb2c4ea429
Shahdu <f1254489174@163.com>
2026-05-06 23:12:30 +0800

    Update test.cpp

--- 快乐数盛水容器/test.cpp
+++ 快乐数盛水容器/test.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 using namespace std;
 namespace HappyMun{
+	//https://leetcode.cn/problems/happy-number/description/
     int BitSquareSum(int n){
         int sum = 0;
         while (n) {
@@ -14,9 +15,9 @@ namespace HappyMun{
     }
 
     bool isHappy(int n) {
-        //���������������Թ�Ϊһ�֣�Ҳ����1֮��2Ȼ��ѭ��������󶼻�ѭ����
-        // ���Կ����ÿ���ָ���ж��Ƿ����+�Ƿ�Ϊ1
-        //���ϸ�֤�����������������޵ģ����Ҷ�����һ��֮��������ô�߶����ظ���
+        //这里的两种情况可以归为一种，也就是1之后到2然后循环，即最后都会循环。
+        // 所以可以用快慢指针判断是否带环+是否为1
+        //（严格证明就是总样本是有限的，在我都遍历一次之后无论怎么走都会重复）
         int slow = n, fast = BitSquareSum(n);
         while (slow != fast && slow != 1) {
             slow = BitSquareSum(slow);
@@ -28,12 +29,13 @@ namespace HappyMun{
 }
 
 namespace WaterContainer{
+		//https://leetcode.cn/problems/container-with-most-water/
     int maxArea(vector<int>& height) {
-        //˼·������������ָ�룬Ȼ��ѡ��С�ľ��������м��ƶ����Ƚ������ֵ
-        //����Ϊɶ����������ŵ�˼·������
-        //1.����������Ե�С����û��ɶ���壬�󲿷�ֱ����������
-        //2.Ȼ����Ǵ����ֱȽϣ������ڶ���ʱ�򣬿��Ƚ��ͣ����Ⱥ�������
-        //3.���ȱ仯 a.��С  b.����  c.��󣨽�����һ����һ���̣��̵Ķ������ģ����������һ���Ļ������ƶ�֮��Ҳ�ǲ��䣨����ȡС��
+        //思路是这样，左右指针，然后选到小的就让他往中间移动，比较留最大值
+        //至于为啥能这样，大概的思路是这样
+        //1.首先这里相对的小数字没有啥意义，大部分直接跳过就行
+        //2.然后就是大数字比较，再往内动的时候，宽度降低，长度后面讨论
+        //3.长度变化 a.变小  b.不变  c.变大（仅限于一个长一个短，短的动到长的），如果二者一样的话，那移动之后也是不变（二者取小）
         int left = 0, right = height.size() - 1;
         int v = 0, t = 0;
         while (left < right) {
